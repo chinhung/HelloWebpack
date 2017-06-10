06a6d589dfcdf48a24d7066dcdf86c83f4c90eac
chinhung <chinhung101@gmail.com>
2017-06-10 11:37:16 +0800

    add my first entry point index.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,13 @@
+var path = require('path');
+
 module.exports = {
-   entry: 'webpack-dev-server/client?http://localhost:8080'
+    entry: [
+       'webpack-dev-server/client?http://localhost:8080',
+       './app/index.js'
+    ],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
 };
 
