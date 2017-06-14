7bd7859736c97581d8e24b67070b83b8dfe0a55c
chinhung <chinhung101@gmail.com>
2017-06-14 23:50:11 +0800

    add one-way-obj

--- app/root/root.component.js
+++ app/root/root.component.js
@@ -10,8 +10,15 @@ function RootComponentController() {
     self.name = 'RootComponentController';
     self.value1 = 1;
     self.value2 = 2;
+    self.obj1 = {
+        name: 'John',
+        innerObj: {age: 10}
+    };
     self.changeToWord = changeToWord;
     self.changeToNumber = changeToNumber;
+    self.toggleObj1 = toggleObj1;
+    self.toggleObj1Name = toggleObj1Name;
+    self.toggleObj1Age = toggleObj1Age;
 
     function changeToWord() {
         self.value1 = 'one';
@@ -21,6 +28,36 @@ function RootComponentController() {
     function changeToNumber() {
         self.value1 = 1;
         self.value2 = 2;
+    }
+
+    function toggleObj1() {
+        if (self.obj1.name == 'Mary') {
+            self.obj1 = {
+                name: 'John',
+                innerObj: {age: 10}
+            };
+        } else {
+            self.obj1 = {
+                name: 'Mary',
+                innerObj: {age: 20}
+            };
+        }
+    }
+
+    function toggleObj1Name() {
+        if (self.obj1.name == 'Mary') {
+            self.obj1.name = 'John';
+        } else {
+            self.obj1.name = 'Mary';
+        }
+    }
+
+    function toggleObj1Age() {
+        if (self.obj1.innerObj.age == 10) {
+            self.obj1.innerObj.age = 20;
+        } else {
+            self.obj1.innerObj.age = 10;
+        }
     }
 }
 
--- app/root/sub/root.sub.component.js
+++ app/root/sub/root.sub.component.js
@@ -1,7 +1,8 @@
 var rootSubComponent = {
     bindings: {
         oneWay: '<',
-        twoWay: '='
+        twoWay: '=',
+        oneWayObj: '<'
     },
     templateUrl: './app/root/sub/root.sub.component.html',
     controller: RootSubComponentController,
@@ -14,6 +15,9 @@ function RootSubComponentController() {
     self.name = 'RootSubComponentController';
     self.changeToWord = changeToWord;
     self.changeToNumber = changeToNumber;
+    self.toggleObj1 = toggleObj1;
+    self.toggleObj1Name = toggleObj1Name;
+    self.toggleObj1Age = toggleObj1Age;
 
     function changeToWord() {
         self.oneWay = 'one';
@@ -23,6 +27,36 @@ function RootSubComponentController() {
     function changeToNumber() {
         self.oneWay = 1;
         self.twoWay = 2;
+    }
+
+    function toggleObj1() {
+        if (self.oneWayObj.name == 'Mary') {
+            self.oneWayObj = {
+                name: 'John',
+                innerObj: {age: 10}
+            };
+        } else {
+            self.oneWayObj = {
+                name: 'Mary',
+                innerObj: {age: 20}
+            };
+        }
+    }
+
+    function toggleObj1Name() {
+        if (self.oneWayObj.name == 'Mary') {
+            self.oneWayObj.name = 'John';
+        } else {
+            self.oneWayObj.name = 'Mary';
+        }
+    }
+
+    function toggleObj1Age() {
+        if (self.oneWayObj.innerObj.age == 10) {
+            self.oneWayObj.innerObj.age = 20;
+        } else {
+            self.oneWayObj.innerObj.age = 10;
+        }
     }
 }
 
